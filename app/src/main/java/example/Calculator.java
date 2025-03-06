9fa06c90293c9c7de2b79ded38f08c26b9a38964
Bilal <bilal.khan1@student.unimelb.edu.au>
2025-03-06 13:15:59 +1100

    Testing My Commit

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,7 +3,7 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
-        // Adding a comment to see if my comitts work
+        // Adding comment seeing if my comitts works
         return a + b;
     }
 
